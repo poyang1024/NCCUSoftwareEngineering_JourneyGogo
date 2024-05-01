ea7a07a8a65da954b97d03c5d5117008112b2b0a
108403525 <aa80876095@gmail.com>
2024-05-01 16:17:08 +0800

    feat: add create first super user in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,27 +1,60 @@
-from fastapi import FastAPI, Depends, HTTPException
-from typing import List, Annotated
-from app import models
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .models import User
 # 引入engine及database設定好的SessionLocal
 from app.database import engine, SessionLocal
-# 引入Session
-from sqlalchemy.orm import Session
 from .routers.api import api_router
 from .config.config import settings
+from .database import engine, SessionLocal, Base
+from .auth.auth import get_hashed_password
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+     # Setup PostgreSQL connection
+    engine 
+    SessionLocal 
 
-app = FastAPI()
-# 在資料庫中建立剛剛models中設定好的資料結構
-models.Base.metadata.create_all(bind=engine)
+    # Dependency to provide database session
+    def get_db():
+        db = SessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
 
-# 每次操作get_db時，db使用SessionLocal中提供的資料與資料庫連線，產生db存儲，完事後關閉
-def get_db():
+    app.dependency_overrides[get_db] = get_db
+
+    # Create tables if they don't exist (assuming models are defined)
+    Base.metadata.create_all(bind=engine)
+
     db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    user = db.query(User).filter(User.email == settings.FIRST_SUPERUSER).first()
+    if not user:
+        hashed_password = get_hashed_password(settings.FIRST_SUPERUSER_PASSWORD)
+        user = User(
+            email=settings.FIRST_SUPERUSER,
+            hashed_password=hashed_password,
+            is_superuser=True,
+        )
+        db.add(user)
+        db.commit()
 
-# 一個db的dependency，可以看做是要操作的db，這裡的Depends對應get_db，get_db對應SessionLocal    
-db_dependency = Annotated[Session, Depends(get_db)]
+    yield
+
+app = FastAPI(
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    lifespan=lifespan,
+)
+
+# Set all CORS enabled origins
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin).rstrip("/") for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)

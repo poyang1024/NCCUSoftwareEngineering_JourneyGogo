6d17ea2de8bd99dd37a675793ae912e7b0e51879
Lydia Cai <lydia.cai@shopback.com>
2024-05-17 00:20:47 +0800

    modify attractiondetail

--- frontend/src/components/Home/AttractionDetails.tsx
+++ frontend/src/components/Home/AttractionDetails.tsx
@@ -9,7 +9,7 @@ import CreateNewFolderOutlinedIcon from '@mui/icons-material/CreateNewFolderOutl
 import { useState } from 'react';
 import { useNavigate, useLocation, Link } from 'react-router-dom'
 // import LoginForm from '../../components/LoginForm.tsx';
-import { Box, Button, Typography, IconButton, Grid } from '@mui/material';
+import { Box, Button, Typography, IconButton, Grid, Divider } from '@mui/material';
 import StarIcon from '@mui/icons-material/Star';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import CallOutlinedIcon from '@mui/icons-material/CallOutlined';
@@ -45,59 +45,48 @@ const AttractionDetails: React.FC<AttractionDetailsProps> = ({ feature, onClose,
 
     return (
         <div style={{ display: 'flex', flexDirection: 'row', gap: '20px', position: 'relative' }}>
-            <div style={{ width: '60%', borderRadius: '10px 0 0 10px', overflow: 'hidden' }}>
-                <img src={img} alt={alt} style={{ width: '100%', height: 'auto' }} />
+            <div style={{ width: '65%', borderRadius: '10px 0 0 10px', overflow: 'hidden' }}>
+                <img src={img} alt={alt} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
             </div>
-            <Box sx={{ width: '40%', padding: '20px', position: 'relative' }}>
+            <Box sx={{ width: '35%', paddingLeft: '15px', paddingRight: '20px', paddingTop: '20px', paddingBottom: '20px', position: 'relative' }}>
                 <IconButton onClick={onClose} sx={{ position: 'absolute', top: '10px', right: '10px', color: 'D9D9D9' }}>
                     <CloseIcon />
                 </IconButton>
-                <Box sx={{ marginTop: '40px' }}> {/* 保證Title在關閉按鈕下方 */}
+                <Box sx={{
+                            maxHeight: '500px', // Set the height you want for the scrollable area
+                            overflowY: 'auto',
+                            paddingRight: '16px', // Optional: Add some right padding for better appearance
+                            marginTop: '40px' // 保證Title在關閉按鈕下方 
+                        }}>
                     <Typography variant="h6" gutterBottom sx={{ fontFamily: 'Noto Sans TC' }}>
                         {title}
                     </Typography>
-                    <Box display="flex" alignItems="center" mb={1}>
-                        <StarIcon style={{ color: '#FFE500' }} />
+                    <Box display="flex" alignItems="center" mb={2}>
+                        <StarIcon sx={{marginRight:'10px'}} style={{ color: '#FFE500' }} />
                         <Typography variant="body1" sx={{ fontFamily: 'Noto Sans TC' }}>
                             {star}
                         </Typography>
                     </Box>
-                    <Grid container direction="row" spacing={1} alignItems="center" sx={{ marginBottom: '20px' }}>
-                        <Grid item>
-                            <IconButton size="small" disableRipple>
-                                <LocationOnOutlinedIcon />
-                            </IconButton>
-                        </Grid>
-                        <Grid item xs>
-                            <Typography variant="body1" sx={{ fontFamily: 'Noto Sans TC', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
-                                {address}
-                            </Typography>
-                        </Grid>
-                        <Grid item>
-                            <IconButton size="small" disableRipple>
-                                <CallOutlinedIcon />
-                            </IconButton>
-                        </Grid>
-                        <Grid item xs>
-                            <Typography variant="body1" sx={{ fontFamily: 'Noto Sans TC', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
-                                {phone}
-                            </Typography>
-                        </Grid>
-                    </Grid>
+                    <Divider/>
+                    <Box display="flex" alignItems="center" mt={2} mb={'12px'}>
+                        <LocationOnOutlinedIcon sx={{marginRight:'10px'}}/>
+                        <Typography variant="body1" sx={{ fontFamily: 'Noto Sans TC', whiteSpace: 'normal'}}>
+                            {address}
+                        </Typography>
+                    </Box>
+                    <Box display="flex" alignItems="center" mt={1} mb={'12px'}>
+                        <CallOutlinedIcon sx={{marginRight:'10px'}}/>
+                        <Typography variant="body1" sx={{ fontFamily: 'Noto Sans TC', whiteSpace: 'normal' }}>
+                            {phone}
+                        </Typography>
+                    </Box>
                     {Object.entries(openingHours).map(([day, hours], index) => (
-                        <Grid container spacing={1} key={day} alignItems="center">
-                            <Grid item>
-                                {index === 0 ? (
-                                    <IconButton>
-                                        <AccessTimeOutlinedIcon />
-                                    </IconButton>
+                        <Box display="flex" alignItems="center" mt={1} mb={2} sx={{paddingRight:'5px'}}>
+                            {index === 0 ? (
+                                    <AccessTimeOutlinedIcon sx={{marginRight:'10px'}}/>
                                 ) : (
-                                    <IconButton style={{ visibility: 'hidden' }}>
-                                        {/* Invisible icon to maintain layout */}
-                                        <AccessTimeOutlinedIcon />
-                                    </IconButton>
+                                    <AccessTimeOutlinedIcon sx={{marginRight:'10px'}} style={{ visibility: 'hidden' }}/>
                                 )}
-                            </Grid>
                             <Grid item>
                                 <Typography variant="body1" sx={{ fontFamily: 'Noto Sans TC', color: index === new Date().getDay() ? 'black' : 'gray' }}>
                                     {day}
@@ -108,36 +97,31 @@ const AttractionDetails: React.FC<AttractionDetailsProps> = ({ feature, onClose,
                                     {hours}
                                 </Typography>
                             </Grid>
-                        </Grid>
+                        </Box>
                     ))}
-                    <Box
-                        sx={{
-                            maxHeight: '120px', // Set the height you want for the scrollable area
-                            overflowY: 'auto',
-                            paddingRight: '16px', // Optional: Add some right padding for better appearance
-                            marginTop: '20px'
-                        }}
-                    >
-                        <Typography variant="subtitle1" sx={{ fontFamily: 'Noto Sans TC', fontWeight: 'bold', marginBottom: '4px' }}>
+                    <Divider/>
+                    <Box mt={2} mb={1}>
+                        <Typography variant="subtitle1" sx={{ fontFamily: 'Noto Sans TC', fontWeight: 'bold', marginBottom: '4px', fontSize:'16px' }}>
                             評論
                         </Typography>
                         {comments.map((comment, index) => (
-                            <Typography key={index} variant="body1" sx={{ fontFamily: 'Noto Sans TC', marginBottom: '8px' }}>
+                            <Typography key={index} variant="body1" sx={{ fontFamily: 'Noto Sans TC', marginBottom: '8px', borderLeft:'solid 5px #E6E8EE', paddingLeft:'10px'}}>
                                 {comment}
                             </Typography>
                         ))}
                     </Box>
-                    <Box display="flex" justifyContent="space-between" alignItems="center" mt={2}>
-                        <IconButton size="small" disableRipple onClick={() => handleClickFavorite(feature.favorite)}>
-                            <div style={{ backgroundColor: '#FFFFFF', borderRadius: '50%', padding: '4px' }}>
-                                {isFavorited ? <FavoriteIcon style={{ color: '#000' }} /> : <FavoriteBorderIcon style={{ color: '#000' }} />}
-                            </div>
+                    
+                </Box>
+                <Box display="flex" justifyContent="space-between" alignItems="center" mt={2}>
+                        <IconButton size="medium" sx={{  boxShadow:'0px 0px 5px 0px rgba(0, 0, 0, 0.25)' }} disableRipple onClick={() => handleClickFavorite(feature.favorite)}>
+                            {isFavorited ? <FavoriteIcon fontSize="small" style={{ color: '#000' }} /> : <FavoriteBorderIcon fontSize="inherit" style={{ color: '#000' }} />}
                         </IconButton>
                         <Button
                             variant="contained"
                             startIcon={<AddCircleOutlineIcon />}
                             sx={{
-                                borderRadius: '12.5px',
+                                height:'40px',
+                                borderRadius: '20px',
                                 fontFamily: 'Noto Sans TC',
                                 textTransform: 'none', // Prevent uppercase transformation
                                 display: 'flex',
@@ -149,7 +133,6 @@ const AttractionDetails: React.FC<AttractionDetailsProps> = ({ feature, onClose,
                             加入行程
                         </Button>
                     </Box>
-                </Box>
             </Box>
         </div>
     );

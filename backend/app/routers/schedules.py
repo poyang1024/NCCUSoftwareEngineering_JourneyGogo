058ce55d9d70a6ff4df2e5ad0e411f2911079073
ycloudo <cces994512@gmail.com>
2024-05-26 17:57:57 +0800

    feat: schedule CRUD except det schedule by id

--- backend/app/routers/schedules.py
+++ backend/app/routers/schedules.py
@@ -2,16 +2,14 @@ from typing import List, Optional, Any
 from fastapi import APIRouter, HTTPException, Body, Depends, Request, status
 from sqlalchemy import and_, or_, any_
 from fastapi.responses import JSONResponse
+from sqlalchemy.exc import IntegrityError
 
 from ..db.db_setup import SessionLocal
-from ..schemas import schedules
-from ..db.models import models
+from ..schemas.schedules import ScheduleCreate, ScheduleResponse, ScheduleAttractions, ScheduleUpdate
+from ..db.models.models import Schedules, ListBase, User
 
 from ..auth.auth import (
-    get_current_login_status,
     get_current_user,
-    get_current_active_superuser,
-    get_current_active_user,
 )
 
 router = APIRouter()
@@ -20,19 +18,19 @@ db = SessionLocal()
 
 @router.post("", response_model=None)
 async def create_schedules_list(
-    schedule: schedules.ScheduleCreate,
-    current_user: models.User = Depends(get_current_login_status)
+    schedule: ScheduleCreate,
+    current_user: User = Depends(get_current_user)
 ):
     """
     Create a new schedule.
     """
     # check if list name exist
-    exist_schedules = db.query(models.Schedules).filter(models.Schedules.name == schedule.name, models.Schedules.user_id == current_user.uuid).first()
+    exist_schedules = db.query(Schedules).filter(Schedules.name == schedule.name, Schedules.user_id == current_user.uuid).first()
     if exist_schedules:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="List name already exist.")
     
 
-    new_schedule = models.Schedules(
+    new_schedule = Schedules(
         name = schedule.name,
         user_id = current_user.uuid,
         start_date = schedule.start_date,
@@ -44,3 +42,62 @@ async def create_schedules_list(
     db.refresh(new_schedule)
 
     return JSONResponse(status_code=status.HTTP_201_CREATED, content={"message": "List is created successfully"})
+
+@router.get("", response_model=List[ScheduleResponse | ScheduleAttractions])
+async def get_schedules(
+    list_id: int | None = None, 
+    current_user: User = Depends(get_current_user)
+):
+    """
+    Get schedules (all / by list_id)
+    """
+    if list_id: # get data by list_id
+        pass 
+    else: # get all schedules
+        res = db.query(Schedules).filter(Schedules.user_id == current_user.uuid).all()
+
+    return res
+
+@router.patch("/{list_id}", response_model=ScheduleResponse)
+async def update_schedule(
+    list_id: int, 
+    update: ScheduleUpdate,
+    current_user: User = Depends(get_current_user)
+):
+    """
+    Upate schedule time, date, or description
+    """
+    # first check if schedule exist
+    schedule = db.query(Schedules).filter(and_(Schedules.user_id == current_user.uuid, Schedules.id == list_id)).first()
+    if schedule is None:
+        raise HTTPException(status_code=404, detail="Schedule not found")
+    
+    update_data = update.model_dump(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(schedule, key, value)
+
+    try:
+        db.commit()
+        db.refresh(schedule)
+        return schedule
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(
+            status_code=400,
+            detail="Schedule with that name already exists.",
+        )
+
+@router.delete("/{list_id}", response_model=ScheduleResponse)
+async def delete_schedule(
+    list_id: int, 
+    current_user: User = Depends(get_current_user)
+):
+    """
+    Delete schedule by id
+    """
+    schedule = db.query(Schedules).filter(and_(Schedules.user_id == current_user.uuid, Schedules.id == list_id)).first()
+    if schedule is None:
+        raise HTTPException(status_code=404, detail="Schedule not found")
+    db.delete(schedule)
+    db.commit()
+    return schedule
--- backend/app/schemas/schedules.py
+++ backend/app/schemas/schedules.py
@@ -1,9 +1,22 @@
 from typing import Optional
 from pydantic import BaseModel
 from uuid import UUID
-from datetime import date
+from datetime import date, datetime
 
 class ScheduleCreate(BaseModel):
     name: str
     start_date: date
     end_date: date
+
+class ScheduleResponse(ScheduleCreate):
+    id: int
+
+class ScheduleAttractions(BaseModel):
+    attravtion_name: str
+    image: str
+    start_time: datetime
+    end_time: datetime
+
+class ScheduleUpdate(ScheduleCreate):
+    description: str
+

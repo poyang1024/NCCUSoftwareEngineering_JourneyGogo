d16f1963558beb84cfcb82200172fe449ae01deb
王博揚 <112356023@g.nccu.edu.tw>
2024-06-11 07:07:03 +0000

    update import_data shell script

--- import_data.sh
+++ import_data.sh
@@ -1,11 +1,20 @@
+#!/bin/bash
+
+# Variables
+PGUSER="postgres"
+PGDB="app"
+ATTRACTIONS_FILE="/tmp/attractions_info_all.csv"
+COMMENTS_FILE="/tmp/comment_all.csv"
+
+# Import data
+psql -U $PGUSER -d $PGDB <<EOF
 COPY attractions(id, city, tag, name, url, rating, comment_amount, address, phone, pic_url, business_hour)
-FROM '/tmp/attractions_info_all.csv'
+FROM '$ATTRACTIONS_FILE'
 DELIMITER ','
 CSV HEADER;
 
 COPY comment(id, attraction_id, content)
-FROM '/tmp/comment_all.csv'
+FROM '$COMMENTS_FILE'
 DELIMITER ','
 CSV HEADER;
-
-\q
+EOF
